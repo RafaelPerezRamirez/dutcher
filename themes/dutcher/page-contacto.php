8126aea2107119d5a2be196f527eb7d350e0364a
rafabrutaldrums <rafael@gafa.mx>
2017-07-18 16:19:21 -0500

    paginas contacto

---
+++ themes/dutcher/page-contacto.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div class="content-somos text-center content-contacto">
+    <div class="avenir-bold uppercase titlespecial">
+        contacto
+    </div>
+    <div class="avenir-normal title ls2">
+        ¿Tienes un comentario o quieres colaborar con nosotros?
+    </div>
+    <div class="avenir-normal title ls2">
+        Escribe a <a href="mailto:hello@dutcher.com" class="avenir-bold">hello@dutcher.com</a>
+    </div>
+    <div class="avenir-normal title ls2">
+        O sigue nuestras redes sociales
+    </div>
+    <a href="#" class="red redfb redcontacto"></a>
+    <a href="#" class="red redtw redcontacto"></a>
+    <a href="#" class="red redinsta redcontacto"></a>
+</div>
+<?php get_footer(); ?>
---
+++ themes/dutcher/page-quienes-somos.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="content-somos">
+    <div class="logo-somos text-center">
+        <object data="<?php echo assets(); ?>/images/logocolorized.svg" type="image/svg+xml"></object>
+        <div class="text-somos avenir-normal text-justify title">
+            Dutcher...Plataforma de medios digitales con contenido original relacionados a diferentes tematicas actuales como
+            tendencias, musica, arte, viajes y cultura.
+            <div class="avenir-bold text-center uppercase vision-dutcher">
+                visión
+            </div>
+            llegar a consolidarnos como una plataforma de medios digitales referente, con alto impacto y con contenido original y de calidad.
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
